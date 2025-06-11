1e8fd16f2198994f1a23588a3456a3c115a03d88
Adrian Thummerer <a.thummerer@umcg.nl>
2025-06-11 12:06:56 +0000

    1000 fixes.....

--- utils.py
+++ utils.py
@@ -403,6 +403,7 @@ def get_model_str(model_name):
     with open('./config/models.json', 'r') as f:
         models = json.load(f)
     for model in models:
+        print(model_name, model)
         if model_name == model:
             return model['model_str'], model["cloud"]
     return None
